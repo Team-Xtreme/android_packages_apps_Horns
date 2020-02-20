83af053edf67766ecf37c42de95ffd5d77203274
rpingat <pingatravi@gmail.com>
2020-02-20 21:59:32 +0530

    Add battery styles and customizations for Android 10 [2/2]

--- src/com/stag/horns/fragments/StatusBarSettings.java
+++ src/com/stag/horns/fragments/StatusBarSettings.java
@@ -38,8 +38,21 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String SHOW_LTE_FOURGEE = "show_lte_fourgee";
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
+    private static final String BATTERY_PERCENTAGE_HIDDEN = "0";
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+    private static final int BATTERY_STYLE_Q = 0;
+    private static final int BATTERY_STYLE_DOTTED_CIRCLE = 1;
+    private static final int BATTERY_STYLE_CIRCLE = 2;
+    private static final int BATTERY_STYLE_TEXT = 3;
+    private static final int BATTERY_STYLE_HIDDEN = 4;
 
     private SwitchPreference mShowLteFourGee;
+    private ListPreference mBatteryPercent;
+    private ListPreference mBatteryStyle;
+    private SwitchPreference mBatteryCharging;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -54,6 +67,14 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
                 Settings.System.SHOW_LTE_FOURGEE, 0) == 1));
         mShowLteFourGee.setOnPreferenceChangeListener(this);
 
+        mBatteryPercent = (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+        mBatteryCharging = (SwitchPreference) findPreference(STATUS_BAR_BATTERY_TEXT_CHARGING);
+        mBatteryStyle = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
+        int batterystyle = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_Q);
+        mBatteryStyle.setOnPreferenceChangeListener(this);
+
+        updateBatteryOptions(batterystyle);
     }
 
     @Override
@@ -63,9 +84,26 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.SHOW_LTE_FOURGEE, value ? 1 : 0);
             return true;
+        } else if (preference == mBatteryStyle) {
+            int value = Integer.parseInt((String) objValue);
+            updateBatteryOptions(value);
+            return true;
         }
 
         return false;
+    }
+
+    private void updateBatteryOptions(int batterystyle) {
+        boolean enabled = batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN;
+        boolean enabledx = batterystyle != BATTERY_STYLE_TEXT;
+        if (batterystyle == BATTERY_STYLE_HIDDEN) {
+            mBatteryPercent.setValue(BATTERY_PERCENTAGE_HIDDEN);
+            mBatteryPercent.setSummary(mBatteryPercent.getEntry());
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
+        }
+        mBatteryCharging.setEnabled(enabledx);
+        mBatteryPercent.setEnabled(enabled);
     }
 
     @Override
